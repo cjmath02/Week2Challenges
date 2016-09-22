15d0fba4e9f0a81656395968d4ef774cd572470a
cjmath02 <cjmath02@gmail.com>
2016-09-22 13:18:10 -0400

    Week 2 Challenge

---
+++ app.js
@@ -0,0 +1,20 @@
+//create function
+function HelloWorld(value){
+	if 
+		(value == "Hello");
+	{
+	return "Hello World"}
+
+	else {
+		return "You didn't say hello"}
+	}
+
+  //control and return statements
+
+//Prompt user for input and store variable
+
+//Call function
+var val = prompt("Type 'Hello'")
+alert(getWelcomeMessage(val));
+
+//Alert user results
